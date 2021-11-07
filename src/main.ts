ea505cfd16a8a91543654be59563f2027fbe6152
Sébastien CAPARROS <sebastiencaparros@gmail.com>
2021-11-07 23:23:19 +0900

    Drawing basic particles on a canvas

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,36 @@
-import { test } from './test';
+import { Particle } from './particle';
 
-document.write(test);
+const canvas = document.createElement('canvas');
+canvas.width = 500;
+canvas.height = 400;
+document.body.appendChild(canvas);
+
+const context = canvas.getContext('2d');
+if (!context) {
+	throw new Error('Got a null context from the canvas.');
+}
+
+const particles = Array<Particle>(300);
+for (let i = 0; i < particles.length; i++) {
+	particles[i] = new Particle(
+		canvas.width * Math.random(),
+		canvas.height * Math.random(),
+		`rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`,
+	);
+}
+
+setInterval(() => {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	for (let i = 0; i < particles.length; i++) {
+		context.fillStyle = particles[i].style;
+		context.beginPath();
+		context.arc(
+			particles[i].x,
+			particles[i].y,
+			Particle.size,
+			0,
+			2 * Math.PI,
+		);
+		context.fill();
+	}
+}, 1000 / 30);
---
+++ src/particle.ts
@@ -0,0 +1,9 @@
+export class Particle {
+	static size = 10;
+
+	constructor(
+		public x: number,
+		public y: number,
+		public style: string,
+	) {}
+}
