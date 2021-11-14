3bb3a315f2555b8495466fb7acc75f8699a6cfaa
Sébastien CAPARROS <sebastiencaparros@gmail.com>
2021-11-14 17:43:01 +0900

    Basic WebGL demo to draw particles

--- src/front/front.ts
+++ src/front/front.ts
@@ -69,10 +69,102 @@ function startRenderingProcess() {
 			const y = sharedData.positionsY[i];
 			const type = particleTypes[sharedData.typeIndexes[i]];
 
-			context.drawImage(type.image, x - radius, y - radius);
+			context.drawImage(
+				type.image,
+				x - radius,
+				y - radius,
+				radius * 2,
+				radius * 2,
+			);
 		}
 
 		window.requestAnimationFrame(draw);
 	}
 	window.requestAnimationFrame(draw);
+
+
+
+
+
+
+
+
+
+
+
+
+
+	const webglCanvas = document.createElement('canvas');
+	webglCanvas.width = config.canvas.width;
+	webglCanvas.height = config.canvas.height;
+	document.body.appendChild(webglCanvas);
+
+	const vertexShaderScript = `
+		attribute float a_position_x;
+		attribute float a_position_y;
+
+		void main(void) {
+			gl_Position = vec4(a_position_x, a_position_y, 0.0, 1.0);
+			gl_PointSize = 10.0;
+		}
+	`;
+	const fragmentShaderScript = `
+		uniform sampler2D uSampler;
+
+		void main(void) {
+			gl_FragColor = texture2D(uSampler, gl_PointCoord);
+		}
+	`;
+
+	const webgl = webglCanvas.getContext("webgl")!;
+	if (!webgl) {
+		throw new Error('Got a null webgl context from the canvas.');
+	}
+
+	const vertexShader = webgl.createShader(webgl.VERTEX_SHADER)!;
+	webgl.shaderSource(vertexShader, vertexShaderScript);
+	webgl.compileShader(vertexShader);
+	if (!webgl.getShaderParameter(vertexShader, webgl.COMPILE_STATUS)) {
+		throw new Error(webgl.getShaderInfoLog(vertexShader) || "Error while compiling the vertex shader.");
+	}
+
+	const fragmentShader = webgl.createShader(webgl.FRAGMENT_SHADER)!;
+	webgl.shaderSource(fragmentShader, fragmentShaderScript);
+	webgl.compileShader(fragmentShader);
+	if (!webgl.getShaderParameter(fragmentShader, webgl.COMPILE_STATUS)) {
+		throw new Error(webgl.getShaderInfoLog(fragmentShader) || "Error while compiling the fragment shader.");
+	}
+
+	const program = webgl.createProgram()!;
+	webgl.attachShader(program, vertexShader);
+	webgl.attachShader(program, fragmentShader);
+	webgl.linkProgram(program);
+	if (!webgl.getProgramParameter(program, webgl.LINK_STATUS)) {
+		throw new Error(webgl.getProgramInfoLog(program) || "Error when creating the WebGL program");
+	}
+	webgl.useProgram(program);
+
+	for (let i = 0; i < particleTypes.length; i++) {
+		particleTypes[i].createWebGLTexture(webgl);
+	}
+
+	const positionXBuffer = webgl.createBuffer();
+	webgl.bindBuffer(webgl.ARRAY_BUFFER, positionXBuffer);
+	webgl.bufferData(webgl.ARRAY_BUFFER, new Float32Array([-0.5, +0.5, 0.0]), webgl.STATIC_DRAW);
+	const positionXAttribute = webgl.getAttribLocation(program, "a_position_x");
+	webgl.enableVertexAttribArray(positionXAttribute);
+	webgl.vertexAttribPointer(positionXAttribute, 1, webgl.FLOAT, false, 0, 0);
+
+	const positionYBuffer = webgl.createBuffer();
+	webgl.bindBuffer(webgl.ARRAY_BUFFER, positionYBuffer);
+	webgl.bufferData(webgl.ARRAY_BUFFER, new Float32Array([-0.5, -0.5, 0.0]), webgl.STATIC_DRAW);
+	const positionYAttribute = webgl.getAttribLocation(program, "a_position_y");
+	webgl.enableVertexAttribArray(positionYAttribute);
+	webgl.vertexAttribPointer(positionYAttribute, 1, webgl.FLOAT, false, 0, 0);
+
+	webgl.activeTexture(webgl.TEXTURE0);
+	webgl.bindTexture(webgl.TEXTURE_2D, particleTypes[0].webglTexture!);
+	webgl.uniform1i(webgl.getUniformLocation(program, 'uSampler'), 0);
+
+	webgl.drawArrays(webgl.POINTS, 0, 3);
 }
--- src/front/particle-type.ts
+++ src/front/particle-type.ts
@@ -1,8 +1,13 @@
 import { SharedParticleType } from '../common/shared-particle-type';
 
+// Must be a power of 2 for WebGL
+// TODO put in config
+export const TEXTURE_PRECISION = 64;
+
 export class ParticleType {
 	public shared: SharedParticleType;
-	public image: CanvasImageSource;
+	public image: HTMLCanvasElement;
+	public webglTexture?: WebGLTexture;
 
 	constructor(
 		shared: SharedParticleType,
@@ -12,8 +17,8 @@ export class ParticleType {
 		this.shared = shared;
 
 		this.image = document.createElement('canvas');
-		this.image.width = radius * 2;
-		this.image.height = radius * 2;
+		this.image.width = TEXTURE_PRECISION;
+		this.image.height = TEXTURE_PRECISION;
 
 		const context = this.image.getContext('2d')!;
 		if (!context) {
@@ -22,7 +27,28 @@ export class ParticleType {
 
 		context.fillStyle = style;
 		context.beginPath();
-		context.arc(radius, radius, radius, 0, 2 * Math.PI);
+		context.arc(
+			TEXTURE_PRECISION / 2,
+			TEXTURE_PRECISION / 2,
+			TEXTURE_PRECISION / 2,
+			0,
+			2 * Math.PI,
+		);
+		context.arc
 		context.fill();
+	}
+
+	createWebGLTexture(webgl: WebGLRenderingContext) {
+		this.webglTexture = webgl.createTexture()!;
+		webgl.bindTexture(webgl.TEXTURE_2D, this.webglTexture);
+		webgl.texImage2D(
+			webgl.TEXTURE_2D,
+			0,
+			webgl.RGBA,
+			webgl.RGBA,
+			webgl.UNSIGNED_BYTE,
+			this.image,
+		);
+		webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_MIN_FILTER, webgl.LINEAR);
 	}
 }
